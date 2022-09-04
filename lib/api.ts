5ceebaedfa2fe4294988b566abdc0e067d52ec69
Netanel Gilad <netanelg@wix.com>
2022-09-04 11:26:56 +0300

    refactor

--- lib/api.ts
+++ lib/api.ts
@@ -1,52 +1,6 @@
 import { apiKeyAuthorizationStrategy, createClient } from "@wix/sdk";
 import { data } from "@wix/data-backend-public-sdk-poc";
 
-const API_URL = process.env.WORDPRESS_API_URL;
-
-async function fetchAPI(query = "", { variables }: Record<string, any> = {}) {
-  const headers = { "Content-Type": "application/json" };
-
-  if (process.env.WORDPRESS_AUTH_REFRESH_TOKEN) {
-    headers[
-      "Authorization"
-    ] = `Bearer ${process.env.WORDPRESS_AUTH_REFRESH_TOKEN}`;
-  }
-
-  // WPGraphQL Plugin must be enabled
-  const res = await fetch(API_URL, {
-    headers,
-    method: "POST",
-    body: JSON.stringify({
-      query,
-      variables,
-    }),
-  });
-
-  const json = await res.json();
-  if (json.errors) {
-    console.error(json.errors);
-    throw new Error("Failed to fetch API");
-  }
-  return json.data;
-}
-
-export async function getPreviewPost(id, idType = "DATABASE_ID") {
-  const data = await fetchAPI(
-    `
-    query PreviewPost($id: ID!, $idType: PostIdType!) {
-      post(id: $id, idType: $idType) {
-        databaseId
-        slug
-        status
-      }
-    }`,
-    {
-      variables: { id, idType },
-    }
-  );
-  return data.post;
-}
-
 export async function getAllPostsWithSlug() {
   return getAllPosts();
 }
@@ -75,4 +29,17 @@ export async function getPostAndMorePosts(slug, preview, previewData) {
     post,
     morePosts,
   };
+}
+
+export async function getHomePageContent() {
+  const client = createClient({ data }).withAuth(
+    apiKeyAuthorizationStrategy(
+      process.env.WIX_API_KEY,
+      process.env.WIX_SITE_ID
+    )
+  );
+
+  return (await client.data.query({
+    collectionName: "HomePage",
+  })).items[0];
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,13 @@
-import { apiKeyAuthorizationStrategy, createClient } from "@wix/sdk";
 import { GetStaticProps } from "next";
 import { Base } from "../components/landing-page/templates/Base";
-import { data } from "@wix/data-backend-public-sdk-poc";
+import { getHomePageContent } from "../lib/api";
 
 const Index = (props: {homePage: any }) => <Base homePage={props.homePage} />;
 
 export default Index;
 
 export const getStaticProps: GetStaticProps = async () => {
-  const client = createClient({ data }).withAuth(
-    apiKeyAuthorizationStrategy(
-      process.env.WIX_API_KEY,
-      process.env.WIX_SITE_ID
-    )
-  );
-
-  const homePage = (await client.data.query({
-    collectionName: "HomePage",
-  })).items[0];
+  const homePage = await getHomePageContent();
 
   return {
     props: { homePage },
